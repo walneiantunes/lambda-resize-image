df14be4054ace4822c169f1fcda76e9541809c8c
Walnei Antunes <walnei.antunes@gmail.com>
2021-11-15 11:38:30 -0300

    Lambda finished

---
+++ README.md
@@ -0,0 +1,4 @@
+**_ lambda-resize-image _**
+This is a lambda function that resizes an image when it is uploaded into a predetermined folder in a bucket.
+
+Attencion: The origin image is deleted from the bucket.
--- handler.js
+++ handler.js
@@ -1,18 +1,63 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const AWS = require("aws-sdk");
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const S3 = new AWS.S3();
+
+const sharp = require("sharp");
+
+const { basename, extname } = require("path");
+
+module.exports.handle = async (event) => {
+  try {
+    await Promise.all(
+      event.Records.map(async (record) => {
+        console.log("RECORD", record);
+
+        const { key } = record.s3.object;
+
+        console.log("KEY", key);
+
+        const image = await S3.getObject({
+          Bucket: process.env.bucket,
+          Key: key,
+        }).promise();
+
+        console.log("image", image);
+
+        const imageBuffer = await sharp(image.Body)
+          .resize(parseInt(process.env.width), parseInt(process.env.height), {
+            fit: "inside",
+            withoutEnlargement: true,
+          })
+          .toFormat("jpeg", {
+            progressive: true,
+            quality: parseInt(process.env.quality),
+          })
+          .toBuffer();
+
+        console.log("BUFFER", imageBuffer);
+
+        await S3.putObject({
+          Body: imageBuffer,
+          Bucket: process.env.bucket,
+          ContentType: "image/jpeg",
+          Key: `${process.env.target}/${process.env.prefix}${basename(
+            key,
+            extname(key)
+          )}${process.env.suffix}.jpg`,
+        }).promise();
+
+        await S3.deleteObject({
+          Bucket: process.env.bucket,
+          Key: key,
+        }).promise();
+      })
+    );
+
+    return true;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
